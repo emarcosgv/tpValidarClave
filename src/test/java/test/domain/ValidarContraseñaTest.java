a2631da1b3f14b1704a43168140a5063bd520b71
emarcosgv <emarcosgv@gmail.com>
2020-04-27 10:44:43 -0300

    first commit

---
+++ src/test/java/test/domain/ValidarContraseñaTest.java
@@ -0,0 +1,55 @@
+package test.domain;
+
+import domain.ValidarContraseña;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ValidarContraseñaTest {
+
+    private ValidarContraseña validarContraseña;
+
+    @Before
+    public void init(){
+        this.validarContraseña = new ValidarContraseña();
+    }
+
+
+    @Test
+    public void claveValida() {
+        this.validarContraseña.setContraseña("PruebaDeContraseñaValida1");
+        Assert.assertTrue(this.validarContraseña.validar());
+    }
+
+    @Test
+    public void claveInvalida() {
+        this.validarContraseña.setContraseña("PruebaDeContraseñaValida1@");
+        Assert.assertFalse(this.validarContraseña.validar());
+    }
+
+    @Test
+    public void claveInvalidaCorta() {
+        this.validarContraseña.setContraseña("@1mM");
+        Assert.assertFalse(this.validarContraseña.validar());
+    }
+    @Test
+    public void claveValidaConEspacios() {
+        this.validarContraseña.setContraseña("PruebaDe ContraseñaValida1");
+        Assert.assertFalse(this.validarContraseña.validar());
+    }
+    @Test
+    public void claveSinDigitos() {
+        this.validarContraseña.setContraseña("PruebaDeContraseñaValida");
+        Assert.assertFalse(this.validarContraseña.validar());
+    }
+    @Test
+    public void claveSinMayusculas() {
+        this.validarContraseña.setContraseña("pruebadecontraseña1");
+        Assert.assertFalse(this.validarContraseña.validar());
+    }
+    @Test
+    public void claveSinMinusculas() {
+        this.validarContraseña.setContraseña("PRUEBADECONTRASEÑA1");
+        Assert.assertFalse(this.validarContraseña.validar());
+    }
+}
